ac9007106851c4a98c8c91f89da6f8cd27a6accb
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-11 19:57:27 -0300

    11DEZ24 - Aula 120 AS - Data Classes.

--- app/src/main/java/com/arttt95/colecoes/testes/classes.kt
+++ app/src/main/java/com/arttt95/colecoes/testes/classes.kt
@@ -1,6 +1,6 @@
 package com.arttt95.colecoes.testes
 
-class Motorista( val nome: String ) {
+/*class Motorista( val nome: String ) {
 
     fun exibirDadosMotorista() = println("Motorista: $nome")
 
@@ -8,20 +8,32 @@ class Motorista( val nome: String ) {
         fun exibirDadosCaminhao() = println("Caminhão: $nomeCaminhao | Motorista: $nome")
     }
 
+}*/
+
+data class Pergunta (val pergunta: String, val respostaCerta: Int){ // Normalmente utilizada para tratar dados
+    /*fun exibir() = println("Exibir")*/
 }
 
 fun main() {
 
-    val motorista = Motorista("Cristiano Penaldo")
+    /*val motorista = Motorista("Cristiano Penaldo")
     val caminhao = motorista.Caminhao("H-112")
     caminhao.exibirDadosCaminhao()
     motorista.exibirDadosMotorista()
 
     //val motorista = Motorista("Lionel Pessi")
-    /*val caminhao = Motorista.Caminhao("H-240")
-    caminhao.exibirDadosCaminhao()*/
+    val caminhao = Motorista.Caminhao("H-240")
+    caminhao.exibirDadosCaminhao()
 
-    /*val motorista = Motorista("Cristiano Penaldo")
+    val motorista = Motorista("Cristiano Penaldo")
     motorista.exibirDadosMotorista()*/
+
+    val pergunta1 = Pergunta("Qual a pergunta?", 1)
+    val pergunta2 = Pergunta("Qual a pergunta?", 2)
+
+    /*println(pergunta1) // Pergunta@682a0b20
+    println(pergunta2) // Pergunta@3d075dc0*/
+
+    println(pergunta1 == pergunta2)
 
 }
