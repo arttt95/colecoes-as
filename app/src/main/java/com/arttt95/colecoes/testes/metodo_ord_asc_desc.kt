48fd0de9bd9be09eea7acde8a808422c8b24fe21
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-15 05:17:42 -0300

    15DEz24 - Aula 123 AS - Método ordenação ASC DESC.

---
+++ app/src/main/java/com/arttt95/colecoes/testes/metodo_ord_asc_desc.kt
@@ -0,0 +1,18 @@
+package com.arttt95.colecoes.testes
+
+fun main() {
+
+    val listaNomes = listOf("Shisui", "Nagato", "Itachi", "Obito", "Hashirama", "Minato")
+    val listaNumeros = listOf(2, 1, 3)
+
+//    val novaListaNumeros = listaNumeros.sorted() // ASC -> a...z & 0...9
+//    val novaListaNumeros = listaNumeros.sortedDescending() // DESC -> z...a & 9...0
+
+//    val novaListaNomes = listaNomes.sorted() // ASC -> a...z & 0...9
+    val novaListaNomes = listaNomes.sortedDescending() // DESC -> z...a & 9...0
+
+    novaListaNomes.forEach { numero ->
+        println(numero)
+    }
+
+}
